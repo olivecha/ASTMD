58189e6d01f421eb89ee1f64d46e4262c7c29c4d
olivecha <78630053+olivecha@users.noreply.github.com>
2021-02-10 14:00:49 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,9 @@
 import ASTMD
 
-Test_Material = ASTMD.D3039(["example.txt"], [12.5], [1], [250], mtr_name="Tensile Test")
+Material_1 = ASTMD.D790(["example.txt"], [25.4], [1], 150, mtr_name="Flexural Test")
+Material_2 = ASTMD.D3039(["example.txt"], [12.5], [1], [250], extensiometer_length=50.8, mtr_name="Tensile Test")
+Material_3 = ASTMD.D5868(["example.txt"], [25.4**2])
+
+
 
 
